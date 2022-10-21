2645dbc5e612709a888e4d995d02c3768d2f7842
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-21 17:52:28 -0500

    work on db.js connection

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jDB =await openDB('jate, 1');
+  const txs = jDb.transaction ('jate', 'readwrite');
+  const store = txs.objectStore.put({ id:1, value: content});
+  const request = store.put ({id:1, value: content});
+  const result = await request;
+  console.log ('data saved', result.value);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  //console.error('putDb not implemented');
+};
+
+export const getDb = async () => {
+  console.log ('GET from database')
+
+
+//connect to target database
+  const jDb =await openDB('jate, 1');
+
+//new instance/specific database and permissions
+  const txs = jDb.transaction ('jate', 'readonly');
+
+//open object store
+  const store = txs.objectStore ('jate');
+
+//get all data
+  const request = store.get(1);
+
+//confirm request
+  const result = await request;
+
+//console.error('getDb not implemented');
+
+result ? (result.value):
+  console.log ('getDb not impletmented')
+return result.value;
+};
 
 initdb();
